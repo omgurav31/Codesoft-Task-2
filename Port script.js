e4ebca2e02229f414e220c74aa0af60eef4f7ea1
OM GURAV <guravom964@gmail.com>
2024-09-04 15:19:24 +0530

    Add files via upload

---
+++ Port script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+            
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Adjust offset for fixed nav
+                behavior: 'smooth'
+            });
+        });
+    });
+});
